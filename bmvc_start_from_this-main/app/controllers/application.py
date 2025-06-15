65014dcac6aa2e7e21fa8d285829fba48aad3285
apptrx <matheusscamargo13@gmail.com>
2025-06-15 19:06:22 -0300

    a

--- bmvc_start_from_this-main/app/controllers/application.py
+++ bmvc_start_from_this-main/app/controllers/application.py
@@ -5,13 +5,17 @@ class Application():
 
     def __init__(self):
         self.pages = {
-        }
-
+            'helper': self.helper,
+            'pagina': self.pagina
+        }   
 
     def render(self,page):
        content = self.pages.get(page, self.helper)
        return content()
 
-
     def helper(self):
         return template('app/views/html/helper')
+    
+    def pagina(self):
+        return template('app/views/html/pagina', noescape=True)
+
