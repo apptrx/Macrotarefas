99601e7cb77a4dd99231e131ea96300e0389c43c
apptrx <matheusscamargo13@gmail.com>
2025-06-29 16:27:40 -0300

    macrotarefa 2

---
+++ bmvc_start_from_this-main/app/controllers/datarecord.py
@@ -0,0 +1,31 @@
+# app/controllers/datarecord.py
+from app.models.evento import Evento
+import json
+
+class DataRecord:
+    def __init__(self):
+        self.eventos = []
+        self.carregar()
+
+    def carregar(self):
+        try:
+            with open("app/controllers/db/eventos.json", "r") as f:
+                dados = json.load(f)
+                self.eventos = [Evento(**item) for item in dados]
+        except FileNotFoundError:
+            self.eventos = []
+
+    def salvar(self):
+        with open("app/controllers/db/eventos.json", "w") as f:
+            json.dump([vars(e) for e in self.eventos], f, indent=2)
+
+    def listar(self):
+        return self.eventos
+
+    def adicionar(self, evento):
+        self.eventos.append(evento)
+        self.salvar()
+
+    def deletar(self, nome):
+        self.eventos = [e for e in self.eventos if e.nome != nome]
+        self.salvar()
