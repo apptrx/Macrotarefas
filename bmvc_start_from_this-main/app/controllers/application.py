c7437ebe24895aced083e73e6206fca424358a68
apptrx <matheusscamargo13@gmail.com>
2025-07-14 20:17:23 -0300

    v2.0

--- bmvc_start_from_this-main/app/controllers/application.py
+++ bmvc_start_from_this-main/app/controllers/application.py
@@ -23,3 +23,29 @@ class Application:
 
     def helper(self):
         return template('app/views/html/helper')
+    
+    def get_session_id(self):
+        return request.get_cookie("session_id")
+
+    def is_authenticated(self, username):
+        return username == self.models.get_username(self.get_session_id())
+
+    def authenticate_user(self, username, password):
+        session_id = self.models.check_user(username, password)
+        if session_id:
+            response.set_cookie("session_id", session_id, httponly=True, secure=False)
+            return session_id
+        return None
+
+    def logout_user(self):
+        self.models.logout(self.get_session_id())
+
+    def portal(self):
+        return template("app/views/html/portal")
+
+    def pagina_restrita(self, username):
+        if self.is_authenticated(username):
+            lista = self.models.listar()
+            return template("app/views/html/pagina", eventos=lista)
+        else:
+            redirect("/portal")
